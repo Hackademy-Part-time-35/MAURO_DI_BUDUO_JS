6770f7d3d0901d0391b56574cd97d5d30a391dc2
mauro.dibuduo <m.dibudu95@gmail.com>
2024-04-08 18:26:24 +0200

    esercitazione js

---
+++ traccia1/calcoli.js
@@ -0,0 +1,14 @@
+let numb1 = 5;
+let numb2 = 10;
+let numb3 = 15;
+let numb4 = 20;
+let numb5 = 25;
+let somma = numb1 + numb2 + numb3 + numb4 + numb5;
+let media = somma / 5;
+console.log(`La somma tra i numeri equivale a ${somma} e la media equivale a ${media}`);
+
+
+
+
+
+
---
+++ traccia2/calcoli.js
@@ -0,0 +1,6 @@
+let anno_corrente = 2024;
+let data_ascita = 1995;
+let età = anno_corrente - data_ascita;
+let anni_mancanti = 100 - età;
+
+console.log(`Hai ${età} anni e ti mancano ${anni_mancanti} anni per compierne 100`);
